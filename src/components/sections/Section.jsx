cd23ca9fd7528920f68e5e79a83f7bedc9f32e99
soares.lopes <soares.lopes@mail.uft.edu.br>
2023-05-30 14:37:06 -0300

    alterando conteúdo do section e header

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -4,11 +4,10 @@ export function Section(){
     return(
         <section className="help-you">
             <div className="main-content">
-                <h1>The National Gambling Support Network</h1>
-                <p>The National Gambling Support Network, formerly known as, 'The National Gambling Treatment Service', is a group of organisations across Great Britain who provide free, confidential and personalised support for anyone who’s experiencing problems from gambling, as well as those affected by someone else’s gambling.</p>
-                <p>If you're worried about your own or someone else's gambling, this service is totally free to access, 24 hours a day, 7 days a week.</p>
+                <h1>Como ajudar a você mesmo</h1>
+                <p>Na seção "Como ajudar a você mesmo", você encontrará recursos e estratégias que podem ser úteis para lidar com o jogo excessivo e promover uma abordagem mais responsável. </p>
                 
-                <h3>Find the right support for you</h3>
+                <h3>Encontre o suporte certo para você</h3>
                 <h2><a href="#">Saiba mais</a></h2>
             </div>
             <div className="main-img">
